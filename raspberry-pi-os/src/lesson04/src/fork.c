71e0cd733968ada4742d39b2008e20f252626c44
KielyMoore <Stopher617@gmail.com>
2022-10-24 18:13:34 -0400

    Priority assignment

--- raspberry-pi-os/src/lesson04/src/fork.c
+++ raspberry-pi-os/src/lesson04/src/fork.c
@@ -2,7 +2,7 @@
 #include "sched.h"
 #include "entry.h"
 
-int copy_process(unsigned long fn, unsigned long arg)
+int copy_process(unsigned long fn, unsigned long arg, unsigned long priority)
 {
 	preempt_disable();
 	struct task_struct *p;
@@ -10,7 +10,7 @@ int copy_process(unsigned long fn, unsigned long arg)
 	p = (struct task_struct *) get_free_page();
 	if (!p)
 		return 1;
-	p->priority = current->priority;
+	p->priority = priority;
 	p->state = TASK_RUNNING;
 	p->counter = p->priority;
 	p->preempt_count = 1; //disable preemtion until schedule_tail
